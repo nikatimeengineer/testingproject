3a086054084500076e05f33cac7b9fbfdbf9d01a
NIka <example@gmail.com>
2022-10-03 12:28:09 +0400

    domashniaia rabota

--- .history/js/script_20221002182419.js
+++ .history/js/script_20221002182419.js
@@ -71,19 +71,9 @@ const personalMovieDB = {
     },
     writeYourGenres: function(){
         for (let i = 1; i <= 3; i++){
-            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-
-            if(genre == '' || genre == null){
-                console.log('Вы ввели некорректные данные блиад');
-                i--
-            } else {
-                personalMovieDB.genres[i - 1] = genre;
-            }
-            
+            const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+            personalMovieDB.genres[i - 1] = genre;
         }
-        personalMovieDB.genres.forEach((item, i) => {
-            console.log(`Любимый жанр ${i + 1} - это ${item}`);
-        })
     }
 };
 
